12b0708696811fafe218085d64339373a661c183
Bianca Maria <biancams_@outlook.com>
2025-10-14 11:46:30 -0300

    :sparkles: Finalização do CRUD do Recurso Postagem

--- src/main/java/com/generation/blogpessoal/Repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/Repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package com.generation.blogpessoal.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -7,5 +9,8 @@ import com.generation.blogpessoal.model.Postagem;
 //Com qual model (tabela) essa Interface irá trabalhar, Long como a chave primária, no nosso caso é o id
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
+	// Assinar método
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
+	// SELECT * FROM tb_postagens WHERE titulo LIKE "%?%;"
 }
--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.Repository.PostagemRepository;
 import com.generation.blogpessoal.model.Postagem;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -30,4 +41,61 @@ public class PostagemController {
 		
 	}
 	
+	@GetMapping("/{id}") // Variável de caminho, ou seja, está no endereço
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		// Pegar o valor no endereço e passar para a variável id
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+				// .orElse(ResponseEntity.notFound().build());
+		// SELECT *FROM tb_postangens WHERE id = ?
+		
+	}
+	
+	// Procurar postagem por título, implementando o método do repository
+		
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getAllByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo)); 
+		
+	}
+	
+	// Para ter resposta HTTP ResponseEntity (é uma classe natural?)
+	// ResquestBody É para pegar algo do corpo da requisição
+	
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){ // Pegar e aplicar as validações de Postagem
+		// Ter acesso ao Objeto de Postagem
+		postagem.setId(null);  // Pois será no DB a criação do ID
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){ // Pegar e aplicar as validações de Postagem
+		// Checar se o post existe ou não
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	// void pois não tem retorno, o retorno é apenas para o usuário
+	@ResponseStatus(HttpStatus.NO_CONTENT)  // retorno caso der certo
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id){
+		
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);
+	}
 }
+
+
+
+
+
+
+
